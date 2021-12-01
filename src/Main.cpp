55eef2eddf290eb108ab8cd48e5a4f39ed9799a2
FrancoisSuzeau <frsuzeau@gmail.com>
2021-12-01 16:51:41 +0100

    Initiate project repository

---
+++ src/Main.cpp
@@ -0,0 +1,29 @@
+/*
+    Name file : Main.cpp
+
+    purpose : Main function of the project
+
+    Author : Francois Suzeau
+
+    Contributor : Vincent Davin
+
+    Creation date : 01/12/2021
+
+    Update : ------
+
+
+*/
+
+#include <iostream>
+
+
+int main(int args, char *argv[])
+{
+    (void) args;
+    (void) argv;
+
+
+    std::cout << ">>> Neural network -- status : Inactive." << std::endl;
+
+    return EXIT_SUCCESS;
+}
